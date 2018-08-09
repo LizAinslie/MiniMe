dbc546fead231fa9f8b2c3c780c33568ac60da40
Landon Gravat <railinator4903@gmail.com>
2018-08-09 01:16:35 +0000

    Finally fixed the numberfact command!

--- commands/numberfact.js
+++ commands/numberfact.js
@@ -2,7 +2,7 @@ const snek = require('snekfetch')
 
 exports.run = (client, msg, args) => {
   snek.get(`http://numbersapi.com/${args[0]}/trivia?default=Boring+number+is+boring.`).then(res => {
-    msg.channel.send(res.body)
+    msg.channel.send(res.body.toString())
   })
 }
 
