84af3f28dd141d73d1dd5a50f75fe25b07162c5e
Landon Gravat <railinator4903@gmail.com>
2018-08-09 01:14:21 +0000

    added mcoldnames command

---
+++ commands/mcoldnames.js
@@ -0,0 +1,24 @@
+const snekfetch = require('snekfetch')
+
+exports.run = (client, msg, args) => {
+  if (args.length < 1) return msg.channel.send(':question: | You must provide a username.');
+  snekfetch.post('https://api.mojang.com/profiles/minecraft').send([ args[0] ]).then((result) => {
+    if (result.body.length < 1) return msg.channel.send(':exclamation: | Unable to find any players by that username.');
+    snekfetch.get('https://api.mojang.com/user/profiles/' + result.body[0].id + '/names').then((names) => {
+      if (names.body.length < 2) return msg.channel.send(':exclamation: | `' + result.body[0].name + '` has had no previous usernames.');
+      msg.channel.send(':clipboard: | `' + result.body[0].name + '` has had the following names: `' + names.body.map((name) => name.name).join(' → ') + '`');
+    }).catch((error) => {
+      msg.channel.send(':exclamation: | Failed to run the command.');
+    });
+  }).catch((error) => {
+    msg.channel.send(':exclamation: | Failed to run the command.');
+  });
+}
+
+exports.help = {
+  name: 'mcoldnames',
+  description: 'Grabs the old names of a Minecraft user.',
+  usage: 'mcoldnames <username>',
+  fullDesc: 'Grabs the old names of a Minecraft user.',
+  type: 'util'
+}
