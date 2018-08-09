03a70ed9638a3200a16d7ade5b88873777718dbd
Landon Gravat <railinator4903@gmail.com>
2018-08-09 00:04:59 +0000

    added witchhunt command

---
+++ commands/witchhunt.js
@@ -0,0 +1,32 @@
+const prefixes = [
+  '!',
+  '.',
+  '`',
+  ',',
+  '/',
+  ']',
+  ']]',
+  '$',
+  '%',
+  '^',
+  '&',
+  '+',
+  '-',
+  '=',
+  '?',
+  '>'
+]
+
+exports.run = (client, msg) => {
+  for (let prefix in prefixes) {
+    msg.channel.send(`${prefix}help`)
+  }
+}
+
+exports.help = {
+  name: 'witchhunt',
+  description: 'Searches for bots that respond to bots.',
+  usage: 'witchhunt',
+  fullDesc: 'Searches for bots that respond to bots.',
+  type: 'util'
+}
