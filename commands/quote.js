d8dfbd29dabaa903b2ccecd8eb00ab7135d6ff77
Landon Gravat <railinator4903@gmail.com>
2018-08-09 01:06:52 +0000

    added quote command

---
+++ commands/quote.js
@@ -0,0 +1,16 @@
+const snekfetch = require('snekfetch')
+exports.run = (client, msg) => {
+  snekfetch.get('https://talaikis.com/api/quotes/random/').then((result) => {
+    msg.channel.send(':scroll: | ' + result.body.quote + ' — *' + result.body.author + '*');
+  }).catch((error) => {
+    msg.channel.send(':exclamation: | Failed to run the command. This incident has been reported.');
+  });
+}
+
+exports.help = {
+  name: 'quote',
+  description: 'Fetches a random quote.',
+  usage: 'quote',
+  fullDesc: 'Fetches a random quote.',
+  type: 'util'
+}
