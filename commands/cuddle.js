10a9282a055237e2c4065d23bf4b4dfbd311aff2
Landon Gravat <railinator4903@gmail.com>
2018-08-10 20:29:53 +0000

    added some more roleplay uwu

---
+++ commands/cuddle.js
@@ -0,0 +1,28 @@
+const snek = require('snekfetch')
+const Discord = require('discord.js')
+const util = require('../util.js')
+
+exports.run = (client, msg, args) => {
+  util.resolveUser(client, args.join(' ')).then(user => {
+    snek.get('https://nekos.life/api/v2/img/cuddle').then(res => {
+      const embed = new Discord.RichEmbed()
+      .setTitle(`${msg.author.username} cuddled ${user.username}.`)
+      .setColor(client.config.color)
+      .setImage(res.body.url)
+    }).catch(err => {
+      msg.channel.send(':exclamation: | Failed to run the command. This incident has been reported.')
+      client.rollbar.error('[cuddle.js] Error getting image from nekos.life: ' + err.message)
+    })
+  }).catch(err => {
+    msg.channel.send(':exclamation: | Failed to run the command. This incident has been reported.')
+    client.rollbar.error('[cuddle.js] Error resolving user: ' + err.message)
+  })
+}
+
+exports.help = {
+  name: 'cuddle',
+  description: 'Cuddle a user.',
+  usage: 'cuddle <user>',
+  fullDesc: 'Cuddle a user.',
+  type: 'rp'
+}
---
+++ commands/slap.js
@@ -0,0 +1,28 @@
+const snek = require('snekfetch')
+const Discord = require('discord.js')
+const util = require('../util.js')
+
+exports.run = (client, msg, args) => {
+  util.resolveUser(client, args.join(' ')).then(user => {
+    snek.get('https://nekos.life/api/v2/img/slap').then(res => {
+      const embed = new Discord.RichEmbed()
+      .setTitle(`${msg.author.username} slapd ${user.username}.`)
+      .setColor(client.config.color)
+      .setImage(res.body.url)
+    }).catch(err => {
+      msg.channel.send(':exclamation: | Failed to run the command. This incident has been reported.')
+      client.rollbar.error('[slap.js] Error getting image from nekos.life: ' + err.message)
+    })
+  }).catch(err => {
+    msg.channel.send(':exclamation: | Failed to run the command. This incident has been reported.')
+    client.rollbar.error('[slap.js] Error resolving user: ' + err.message)
+  })
+}
+
+exports.help = {
+  name: 'slap',
+  description: 'Slap a user.',
+  usage: 'slap <user>',
+  fullDesc: 'Slap a user.',
+  type: 'rp'
+}
