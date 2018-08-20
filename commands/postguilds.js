b39dc1efb3f8f1f9ddd4f3a63115b96edbfc9318
Landon Gravat <railinator4903@gmail.com>
2018-08-20 16:26:55 +0000

    added rule34 command

--- commands/postguilds.js
+++ commands/postguilds.js
@@ -2,15 +2,19 @@ const snekfetch = require('snekfetch')
 
 exports.run = (client, msg) => {
   if (msg.author.id != client.config.ownerID) return msg.channel.send(':no_entry_sign: | Only my developer can use this!')
-  snekfetch.post(`https://discordbots.org/api/bots/${client.user.id}/stats`)
-  .set('Authorization', client.config.apis.botlists.dbl)
+  snekfetch.post(`https://discordbots.org/api/bots/${client.user.id}`)
+  .set('Authorization', client.config.apis.bls)
   .send({
     server_count: client.guilds.size
   }).then(() => {
-    msg.channel.send(':white_check_mark: | Updated **discordbots.org** stats.')
-    console.log('Updated discordbots.org stats.')
+    msg.channel.send(':white_check_mark: | Updated **botlist.space** stats.').catch(err => {
+      client.rollbar.error(`Error sending message in channel with id: ${msg.channel.id} | ${err}`)
+    })
+    console.log('Updated botlist.space stats.')
   }).catch(err => {
-    msg.channel.send(':exclamation: | Failed to update **discordbots.org** stats.')
+    msg.channel.send(':exclamation: | Failed to update **botlist.space** stats.').catch(err => {
+      client.rollbar.error(`Error sending message in channel with id: ${msg.channel.id} | ${err}`)
+    })
     client.rollbar.error(`Discordbots.org server count update failed : ${err}`)
   })
 }
---
+++ commands/r34.js
@@ -0,0 +1,58 @@
+const randomPuppy = require('random-puppy')
+const snekfetch = require('snekfetch')
+
+exports.run = (client, msg) => {
+  if (!msg.channel.nsfw) return msg.channel.send(':exclamation: | You can only run this command in a NSFW channel!');
+  snekfetch.get(`https://botlist.space/api/bots/${client.user.id}/upvotes?ids=true`)
+  .set('Authorization', client.config.apis.bls)
+  .end((err, res) => {
+    var check = res.body.includes(msg.author.id)
+    if (check == 1) {
+      randomPuppy('rule34').then(url => {
+        msg.channel.send({
+          embed: {
+            author: {
+              icon_url: msg.author.displayAvatarURL,
+              name: `Rule34 | Requested by ${msg.author.username}#${msg.author.discriminator}`
+            },
+            color: client.config.color,
+            image: {
+              url: url
+            },
+            timestamp: new Date(),
+            footer: {
+              icon_url: client.user.avatarURL,
+              text: "Status: 200"
+            }
+          }
+        })
+      })
+    } else {
+      msg.channel.send({
+        embed: {
+          title: "Upvoters-Only Command",
+          url: "https://botlist.space/view/456926578228723724",
+          description: "This command is available only for upvoters",
+          fields: [{
+              name: "Go upvote at",
+              value: "https://botlist.space/view/456926578228723724"
+            }
+          ],
+          timestamp: new Date(),
+          footer: {
+            icon_url: client.user.avatarURL,
+            text: "Status: 403"
+          }
+        }
+      })
+    }
+  })
+}
+
+exports.help = {
+  name: 'r34',
+  description: 'Provides Rule34 porn.',
+  usage: 'r34',
+  fullDesc: 'Provides Rule34 porn.',
+  type: 'nsfw'
+}
