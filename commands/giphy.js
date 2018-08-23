4919a5b929cb844bc2b37ceaf122299e6a0a28ea
Landon Gravat <railinator4903@gmail.com>
2018-08-23 20:07:42 +0000

    made sure giphy could only be run in nsfw channels

--- commands/giphy.js
+++ commands/giphy.js
@@ -1,4 +1,5 @@
 exports.run = (client, msg, args) => {
+  if (!msg.channel.nsfw) return msg.channel.send(':exclamation: │ You can only run this command in a NSFW channel!')
   const gif = require('giphy-api')(client.config.apis.giphy)
   const search = args.join(' ')
   gif.search({
