b49f95015b96a4bcb621c0f1715a77dc842d659d
therailor <therailor@railor-music.com>
2018-09-05 11:01:23 -0400

    added update command

---
+++ commands/update.js
@@ -0,0 +1,18 @@
+const execute = require('child_process')
+
+exports.run = (client, msg) => {
+  if (msg.author.id !== client.config.ownerID) return msg.channel.send(':no_entry_sign: │ You are not my developer!')
+  execute.exec('git pull && pm2 restart 1', (error, stdout, stderr) => {
+    if (error) { return console.error(error) }
+		const result = formatArbitrary(client, stderr || stdout)
+		if (result.length > 1992) {
+			uploadToHastebin(result).then((url) => {
+				msg.channel.send(':outbox_tray: │ ' + url)
+			}).catch((error) => {
+				msg.channel.send(':exclamation: │ Failed to upload result to hastebin. `' + error.message + '`')
+			})
+		} else {
+			msg.channel.send(':inbox_tray: │ Pulled from GitHub:\n```bash\n' + result + '```')
+		}
+	})
+}
