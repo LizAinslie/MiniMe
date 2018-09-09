29fdf274aec467c77a2f4fabcf505d14aaec05d7
Landon Gravat <304961@sps.org>
2018-09-09 17:53:01 +0000

    moved channelCreate event over to rethink

--- events/channelCreate.js
+++ events/channelCreate.js
@@ -2,34 +2,37 @@ const util = require('../util.js')
 const dateformat = require('dateformat')
 
 module.exports = async (client, channel) => {
-  if (!client.guildSettings.has(channel.guild.id)) return
-  if (!client.guildSettings.getProp(channel.guild.id, 'doLogs')) return
-  const entry = await channel.guild.fetchAuditLogs({type: 'CHANNEL_CREATE'}).then(audit => audit.entries.first())
-
-  util.log(client, channel, {
-    embed: {
-      title: 'Channel Create',
-      color: client.config.color,
-      thumbnail: {
-        url: entry.executor.avatarURL
-      },
-      fields: [
-        {
-          name: 'Executor:',
-          value: entry.executor.username + '#' + entry.executor.discriminator,
-          inline: true
+  client.r.table('serverSettings').get(channel.guild.id).run(async(error, settings) => {
+    if (!settings) return
+    if (!settings.doLogs) return
+    const entry = await channel.guild.fetchAuditLogs({type: 'CHANNEL_CREATE'}).then(audit => audit.entries.first())
+    
+    const logChannel = channel.guild.channels.get(settings.logChannel)
+    logChannel.send({
+      embed: {
+        title: 'Channel Create',
+        color: client.config.color,
+        thumbnail: {
+          url: entry.executor.avatarURL
         },
-        {
-          name: 'Channel:',
-          value: `${channel}`,
-          inline: true
-        },
-        {
-          name: 'Time:',
-          value: dateformat(entry.createdTimestamp, 'mm/dd/yy hh:MM:ss TT'),
-          inline: true
-        }
-      ]
-    }
+        fields: [
+          {
+            name: 'Executor:',
+            value: entry.executor.username + '#' + entry.executor.discriminator,
+            inline: true
+          },
+          {
+            name: 'Channel:',
+            value: `${channel}`,
+            inline: true
+          },
+          {
+            name: 'Time:',
+            value: dateformat(entry.createdTimestamp, 'mm/dd/yy hh:MM:ss TT'),
+            inline: true
+          }
+        ]
+      }
+    })
   })
 }
