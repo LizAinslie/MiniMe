75823c2b4f6a11ecc86a4a306ca70a98b03a51ce
Landon Gravat <304961@sps.org>
2018-09-09 03:26:19 +0000

    fixed balance

--- commands/balance.js
+++ commands/balance.js
@@ -4,12 +4,14 @@ exports.run = (client, msg, args) => {
   if (args[0]) {
     resolveUser(client, args.join(' ')).then(user => {
       client.r.table('balance').get(user.id).run((error, balance) => {
-        msg.channel.send(`${user.username}#${user.discriminator} has **<:coins:482589075459801098>` + balance ? balance.amount.toString() : 0 + ' Minicoins** in their bank account.')
+        const bal = balance ? balance.amount.toString() : 0
+        msg.channel.send(`${user.username}#${user.discriminator} has **<:coins:482589075459801098>` + bal + ' Minicoins** in their bank account.')
 			});
     })
   } else {
     client.r.table('balance').get(msg.author.id).run((error, balance) => {
-      msg.channel.send('You have **<:coins:482589075459801098>' + balance ? balance.amount.toString() : 0 + ' Minicoins** in your bank account.')
+      const bal = balance ? balance.amount.toString() : 0
+      msg.channel.send('You have **<:coins:482589075459801098>' + bal + ' Minicoins** in your bank account.')
 		});
   }
 }
