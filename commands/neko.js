6649b9c4d7e1b2b58681e0414c3e1ddc3dba9f33
Landon Gravat <304961@sps.org>
2018-09-09 05:01:52 +0000

    added more outputs to neko cmd

--- commands/neko.js
+++ commands/neko.js
@@ -29,16 +29,34 @@ exports.run = async (client, msg, args) => {
   let body
   switch (args[0].toLowerCase()) {
     case 'solo':
-      body = await snekfetch.get(apiBase + 'solo')
+      body = await JSON.parse(JSON.stringify(await neko.getNSFWGirlSoloGif()))
       msg.channel.send({
         embed: embed(client, msg, body.url)
       })
       break
     case 'femdom':
-      body = await JSON.parse(JSON.stringify(await neko.getNSFWGirlSoloGif()))
+      body = await JSON.parse(JSON.stringify(await neko.getNSFWFemdom()))
       msg.channel.send({
         embed: embed(client, msg, body.url)
       })
+      break
+    case 'pussy':
+      if (args[1] && args[1] === 'gif') {
+        body = await JSON.parse(JSON.stringify(await neko.getNSFWPussyGif()))
+        msg.channel.send({
+          embed: embed(client, msg, body.url)
+        })
+      } else if (args[1] && args[1] === 'wank') {
+        body = await JSON.parse(JSON.stringify(await neko.getNSFWPussyWankGif()))
+        msg.channel.send({
+          embed: embed(client, msg, body.url)
+        })
+      }else {
+        body = await JSON.parse(JSON.stringify(await neko.getNSFWPussy()))
+        msg.channel.send({
+          embed: embed(client, msg, body.url)
+        })
+      }
       break
     default:
       msg.channel.send(':interrobang: │ That is not a valid Neko type!')
