16f95db7b92d62d1fa7b4c46d3fbb7cc46fdb989
Landon Gravat <304961@sps.org>
2018-09-09 17:12:09 +0000

    moved server settings to rethink

--- commands/options.js
+++ commands/options.js
@@ -2,63 +2,65 @@ const Discord = require('discord.js')
 const getEmbedColor = require('../util/getHighestRoleColor.js')
 
 exports.run = (client, msg, args) => {
-  if (client.guildSettings.has(msg.guild.id)) {
-    if (!msg.member.hasPermission('MANAGE_SERVER')) return msg.channel.send(':no_entry_sign: │ You do not have permission to use this! you need `MANAGE_SERVER` permission.')
-    switch(args[0].toLowerCase()) {
-      case 'welcomes':
-        let welcomes = Boolean(parseInt(args[1].trim(), 10))
-        client.r.table('serverSettings').get(msg.guild.id).update({
-          doWelcomes: welcomes
-        })
-        msg.channel.send({
-          embed: {
-            title: 'Server Options Updated',
-            color: getEmbedColor(msg),
-            fields: [
-              {
-                name: 'Value Changed',
-                value: 'Welcomes',
-                inline: true
-              },
-              {
-                name: 'Changed to',
-                value: welcomes.toString(),
-                inline: true
-              }
-            ]
-          }
-        })
-        break
-      case 'logs':
-        let logs = Boolean(parseInt(args[0].trim(), 10))
-        client.r.table('serverSettings').get(msg.guild.id).update({
-          doLogs: logs
-        })
-        msg.channel.send({
-          embed: {
-            title: 'Server Options Updated',
-            color: getEmbedColor(msg),
-            fields: [
-              {
-                name: 'Value Changed',
-                value: 'Logs',
-                inline: true
-              },
-              {
-                name: 'Changed to',
-                value: logs.toString(),
-                inline: true
-              }
-            ]
-          }
-        })
-        break
-      default:
-        msg.channel.send(':interrobang: │ You need to specify either `logs` or `welcomes` as a value to change!')
+  client.r.table('balance').get(msg.guild.id).run((error, settings) => {
+    if (settings) {
+      if (!msg.member.hasPermission('MANAGE_SERVER')) return msg.channel.send(':no_entry_sign: │ You do not have permission to use this! you need `MANAGE_SERVER` permission.')
+      switch(args[0].toLowerCase()) {
+        case 'welcomes':
+          let welcomes = Boolean(parseInt(args[1].trim(), 10))
+          client.r.table('serverSettings').get(msg.guild.id).update({
+            doWelcomes: welcomes
+          })
+          msg.channel.send({
+            embed: {
+              title: 'Server Options Updated',
+              color: getEmbedColor(msg),
+              fields: [
+                {
+                  name: 'Value Changed',
+                  value: 'Welcomes',
+                  inline: true
+                },
+                {
+                  name: 'Changed to',
+                  value: welcomes.toString(),
+                  inline: true
+                }
+              ]
+            }
+          })
+          break
+        case 'logs':
+          let logs = Boolean(parseInt(args[0].trim(), 10))
+          client.r.table('serverSettings').get(msg.guild.id).update({
+            doLogs: logs
+          })
+          msg.channel.send({
+            embed: {
+              title: 'Server Options Updated',
+              color: getEmbedColor(msg),
+              fields: [
+                {
+                  name: 'Value Changed',
+                  value: 'Logs',
+                  inline: true
+                },
+                {
+                  name: 'Changed to',
+                  value: logs.toString(),
+                  inline: true
+                }
+              ]
+            }
+          })
+          break
+        default:
+          msg.channel.send(':interrobang: │ You need to specify either `logs` or `welcomes` as a value to change!')
+      }
+    } else {
+      msg.channel.send(':exclamation: │ Please run `' + client.config.prefix + 'setup` to setup your server first!')
     }
-  } else {
-    msg.channel.send(':exclamation: │ Please run `' + client.config.prefix + 'setup` to setup your server first!')
-  }
+  })
 }
 
 exports.help = {
