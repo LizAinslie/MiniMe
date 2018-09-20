c9fa9c5c65e0d1689f18e4b975b748567c3f9ce4
Landon Gravat <railinator4903@gmail.com>
2018-09-20 17:38:55 -0500

    modified big avatar function and made jpeg command dynamic

--- commands/needsjpeg.js
+++ commands/needsjpeg.js
@@ -1,20 +1,34 @@
 const resolveUser = require('../util/resolveUser.js')
 const getEmbedColor = require('../util/getHighestRoleColor.js')
 const snekfetch = require('snekfetch')
+const getBigAvatar = require('../util/getBigAvatar.js')
 
 exports.run = (client, msg, args) => {
-  resolveUser(client, args.join(' ')).then(async user => {
-    const {body: buffer } = await snekfetch.get(`https://api.railrunner16.me/api/gen/needsjpeg?url=${user.avatarURL}`)
-    msg.channel.createMessage({
-      embed: {
-        title: `${user.username} needs more JPEG!`,
-        color: getEmbedColor(client, msg),
-        image: {
-          url: 'attachment://jpeg.jpeg'
+  if (!args[0]) {
+    resolveUser(client, args.join(' ')).then(async user => {
+      const { body: buffer } = await snekfetch.get(`https://api.railrunner16.me/api/gen/needsjpeg?url=${getBigAvatar(user)}`)
+      msg.channel.createMessage({
+        embed: {
+          title: `${user.username} needs more JPEG!`,
+          color: getEmbedColor(client, msg),
+          image: {
+            url: 'attachment://jpeg.jpeg'
+          }
         }
-      }
-    }, { file: buffer, name: 'jpeg.jpeg' })
-  })
+      }, { file: buffer, name: 'jpeg.jpeg' })
+    })
+  } else {
+    const { body: buffer } = await snekfetch.get(`https://api.railrunner16.me/api/gen/needsjpeg?url=${getBigAvatar(msg.author)}`)
+      msg.channel.createMessage({
+        embed: {
+          title: `${msg.author.username} needs more JPEG!`,
+          color: getEmbedColor(client, msg),
+          image: {
+            url: 'attachment://jpeg.jpeg'
+          }
+        }
+      }, { file: buffer, name: 'jpeg.jpeg' })
+  }
 }
 
 exports.help = {
