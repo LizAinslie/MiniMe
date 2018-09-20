bdbc77d57505c28e0f3491b3f23421add0c10fe4
Landon Gravat <railinator4903@gmail.com>
2018-09-20 16:39:06 -0500

    fixed needsjpeg to use https://api.railrunner16.me instead of making image manipulations locally

--- commands/needsjpeg.js
+++ commands/needsjpeg.js
@@ -1,26 +1,19 @@
 const resolveUser = require('../util/resolveUser.js')
 const getEmbedColor = require('../util/getHighestRoleColor.js')
-var Jimp = require('jimp')
+const snekfetch = require('snekfetch')
 
 exports.run = (client, msg, args) => {
-  resolveUser(client, args.join(' ')).then(user => {
-    Jimp.read(user.avatarURL, (err, img) => {
-      if (err) throw err
-      img.quality(3)
-      .getBuffer(Jimp.MIME_JPEG, (err, buffer) => {
-        if (err) throw err
-        msg.channel.createMessage({
-          embed: {
-            title: `${user.username} needs more JPEG!`,
-            color: getEmbedColor(client, msg),
-            image: {
-              // url: `https://api.railrunner16.me/api/gen/needsjpeg?url=${user.avatarURL}`
-              url: 'attachment://jpeg.jpeg'
-            }
-          }
-        }, { file: buffer, name: 'jpeg.jpeg' })
-      })
-    })
+  resolveUser(client, args.join(' ')).then(async user => {
+    const {body: buffer } = await snekfetch.get(`https://api.railrunner16.me/api/gen/needsjpeg?url=${user.avatarURL}`)
+    msg.channel.createMessage({
+      embed: {
+        title: `${user.username} needs more JPEG!`,
+        color: getEmbedColor(client, msg),
+        image: {
+          url: 'attachment://jpeg.jpeg'
+        }
+      }
+    }, { file: buffer, name: 'jpeg.jpeg' })
   })
 }
 
