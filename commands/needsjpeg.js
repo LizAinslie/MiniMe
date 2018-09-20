e9278060de34bbf0fee879114c0ee1ec2a70b8e4
Landon Gravat <304961@sps.org>
2018-09-20 19:10:07 +0000

    fixed some commands

--- commands/needsjpeg.js
+++ commands/needsjpeg.js
@@ -1,16 +1,25 @@
 const resolveUser = require('../util/resolveUser.js')
 const getEmbedColor = require('../util/getHighestRoleColor.js')
+var Jimp = require('jimp')
 
 exports.run = (client, msg, args) => {
   resolveUser(client, args.join(' ')).then(user => {
-    msg.channel.createMessage({
-      embed: {
-        title: `${user.username} needs more JPEG!`,
-        color: getEmbedColor(client, msg),
-        image: {
-          url: `https://api.railrunner16.me/api/gen/needsjpeg?url=${user.avatarURL}`
-        }
-      }
+    Jimp.read(user.avatarURL, (err, img) => {
+      if (err) throw err
+      img.quality(3)
+      .getBuffer(Jimp.MIME_JPEG, (err, buffer) => {
+        if (err) throw err
+        msg.channel.createMessage({
+          embed: {
+            title: `${user.username} needs more JPEG!`,
+            color: getEmbedColor(client, msg),
+            image: {
+              // url: `https://api.railrunner16.me/api/gen/needsjpeg?url=${user.avatarURL}`
+              url: 'attachment://jpeg.jpeg'
+            }
+          }
+        }, { file: buffer, name: 'jpeg.jpeg' })
+      })
     })
   })
 }
