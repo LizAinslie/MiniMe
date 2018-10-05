f6cdaf306d498459d91952379a354d2276e56c6d
railinator4903 <railinator4903@gmail.com>
2018-10-05 15:04:52 +0000

    fix role color util

--- util/getHighestRoleColor.js
+++ util/getHighestRoleColor.js
@@ -1,6 +1,6 @@
 module.exports = (client, msg) => {
   let color = 0
-  if (msg.guild) {
+  if (msg.channel.guild) {
     const roles = msg.channel.guild.members.get(client.user.id).roles.sort((a, b) => a.position > b.position ? -1 : 1)
     if (roles.length > 0) {
       color = roles[0].color
