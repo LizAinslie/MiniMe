e75390a616a38d36d5fb32eb4d4ade9462a686ae
railinator4903 <railinator4903@gmail.com>
2018-10-05 18:12:15 +0000

    re-enabled dashboard

--- events/shardReady.js
+++ events/shardReady.js
@@ -1,6 +1,6 @@
 const dashboard = require('../addons/dashboard.js')
 
-module.exports = client => {
+module.exports = (client, id) => {
   // console.log(`Ready to run in ${client.channels.size} channels on ${client.guilds.size} servers, for a total of ${client.users.size} users.`)
   client.edidStatus('online', { name: 'to Dr. Evil laugh', type: 2 })
 
