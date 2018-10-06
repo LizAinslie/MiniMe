229443626350dea3d8d42d4acf2a9d7ea4c62687
railinator4903 <railinator4903@gmail.com>
2018-10-06 22:05:55 +0000

    lockdown variables in eval better

--- util/formatArbitrary.js
+++ util/formatArbitrary.js
@@ -2,5 +2,15 @@ module.exports = (client, data) => {
 	data = data.replace(new RegExp(client.config.token, 'g'), '-- SENSITIVE INFORMATION --')
 	data = data.replace(new RegExp(client.config.dashboard.secret, 'g'), '-- SENSITIVE INFORMATION --')
 	data = data.replace(new RegExp(client.config.dashboard.sessionSecret, 'g'), '-- SENSITIVE INFORMATION --')
+	data = data.replace(new RegExp(client.config.apis.botlists.bls, 'g'), '-- SENSITIVE INFORMATION --')
+	data = data.replace(new RegExp(client.config.apis.botlists.dbl, 'g'), '-- SENSITIVE INFORMATION --')
+	data = data.replace(new RegExp(client.config.apis.botlists.dbg, 'g'), '-- SENSITIVE INFORMATION --')
+	data = data.replace(new RegExp(client.config.apis.triggered, 'g'), '-- SENSITIVE INFORMATION --')
+	data = data.replace(new RegExp(client.config.apis.idiot, 'g'), '-- SENSITIVE INFORMATION --')
+	data = data.replace(new RegExp(client.config.apis.yt, 'g'), '-- SENSITIVE INFORMATION --')
+	data = data.replace(new RegExp(client.config.apis.rollbar, 'g'), '-- SENSITIVE INFORMATION --')
+	data = data.replace(new RegExp(client.config.apis.openweathermap, 'g'), '-- SENSITIVE INFORMATION --')
+	data = data.replace(new RegExp(client.config.apis.fortnite, 'g'), '-- SENSITIVE INFORMATION --')
+	data = data.replace(new RegExp(client.config.apis.giphy, 'g'), '-- SENSITIVE INFORMATION --')
 	return data
 }
