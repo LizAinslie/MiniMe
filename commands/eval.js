e54a4d5006eadc1b20e47fd5e65b8de1f6a2a578
railinator4903 <railinator4903@gmail.com>
2018-10-06 21:50:14 +0000

    made developers a database value

--- commands/eval.js
+++ commands/eval.js
@@ -5,24 +5,26 @@ const formatArbitrary = require('../util/formatArbitrary.js')
 const uploadToHastebin = require('../util/uploadToHastebin.js')
 const util = require('util')
 
-exports.run = async (client, msg, args) => {
-  if (msg.author.id !== client.config.ownerID) { return msg.channel.createMessage(':no_entry_sign: │ Only my developer can use this!') }
-  try {
-				let result = await eval(args.join(' '))
-				if (typeof result !== 'string') result = util.inspect(result)
-				result = formatArbitrary(client, result)
-				if (result.length > 1992) {
-					uploadToHastebin(result).then((url) => {
-						msg.channel.createMessage(':outbox_tray:   **»**   ' + url)
-					}).catch((error) => {
-						msg.channel.createMessage(':exclamation:   **»**   Failed to upload result to hastebin. `' + error.message + '`')
-					})
-				} else {
-					msg.channel.createMessage('```js\n' + result + '```')
-				}
-			} catch (e) {
-				msg.channel.createMessage('```js\n' + e + '```')
+exports.run = (client, msg, args) => {
+  client.r.table('users').get(msg.author.id).run().then(async user => {
+	  if (!user.developer) { return msg.channel.createMessage(':no_entry_sign: │ Only my developer can use this!') }
+	  try {
+			let result = await eval(args.join(' '))
+			if (typeof result !== 'string') result = util.inspect(result)
+			result = formatArbitrary(client, result)
+			if (result.length > 1992) {
+				uploadToHastebin(result).then((url) => {
+					msg.channel.createMessage(':outbox_tray:   **»**   ' + url)
+				}).catch((error) => {
+					msg.channel.createMessage(':exclamation:   **»**   Failed to upload result to hastebin. `' + error.message + '`')
+				})
+			} else {
+				msg.channel.createMessage('```js\n' + result + '```')
 			}
+		} catch (e) {
+			msg.channel.createMessage('```js\n' + e + '```')
+		}
+  })
 }
 
 exports.help = {
--- commands/reload.js
+++ commands/reload.js
@@ -1,13 +1,15 @@
 /* Eris Fixed */
 
 exports.run = (client, message, args) => {
-  if (message.author.id !== client.config.ownerID) return message.reply('Only my developer can use this command.')
-  if (!args || args.size < 1) return message.reply('Must provide a command name to reload.')
-  // the path is relative to the *current folder*, so just ./filename.js
-  if (args[0].toLowerCase() === 'music') return delete require.cache[require.resolve(`../music.js`)]
-  if (args[0].toLowerCase() === 'config') return delete require.cache[require.resolve(`../config.json`)]
-  delete require.cache[require.resolve(`./${args[0]}.js`)]
-  message.reply(`The command ${args[0]} has been reloaded`)
+  client.r.table('users').get(message.author.id).run().then(user => {
+    if (!user.developer) return message.reply('Only my developer can use this command.')
+    if (!args || args.size < 1) return message.reply('Must provide a command name to reload.')
+    // the path is relative to the *current folder*, so just ./filename.js
+    if (args[0].toLowerCase() === 'music') return delete require.cache[require.resolve(`../music.js`)]
+    if (args[0].toLowerCase() === 'config') return delete require.cache[require.resolve(`../config.json`)]
+    delete require.cache[require.resolve(`./${args[0]}.js`)]
+    message.reply(`The command ${args[0]} has been reloaded`)
+  })
 }
 
 exports.help = {
