767d8162dec1242124af23783421446d73704c12
railinator4903 <railinator4903@gmail.com>
2018-10-06 20:48:20 +0000

    fixed reddit

--- commands/reddit.js
+++ commands/reddit.js
@@ -1,10 +1,10 @@
 const snekfetch = require('snekfetch')
 
 exports.run = (client, msg, args) => {
-  if (args.length < 1) return msg.channel.send(':question: │ You must provide a subreddit.')
+  if (args.length < 1) return msg.channel.createMessage(':question: │ You must provide a subreddit.')
   snekfetch.get('https://www.reddit.com/r/' + args[0] + '/about.json').then((result) => {
-    if (!('display_name' in result.body.data)) return msg.channel.send(':exclamation: │ Unable to find a subreddit by that name.') // dumb dumb reddit
-    msg.channel.send({
+    if (!('display_name' in result.body.data)) return msg.channel.createMessage(':exclamation: │ Unable to find a subreddit by that name.') // dumb dumb reddit
+    msg.channel.createMessage({
       embed: {
         title: result.body.data.display_name,
         color: this.bot.embedColor,
@@ -16,8 +16,8 @@ exports.run = (client, msg, args) => {
       }
     })
   }).catch((error) => {
-    if (error.statusCode === 404) return msg.channel.send(':exclamation: │ Unable to find a subreddit by that name.')
-    msg.channel.send(':exclamation: │ Failed to run the command. This incident has been reported.')
+    if (error.statusCode === 404) return msg.channel.createMessage(':exclamation: │ Unable to find a subreddit by that name.')
+    msg.channel.createMessage(':exclamation: │ Failed to run the command. This incident has been reported.')
     client.rollbar.error('Failed to get subreddit: ' + error)
   })
 }
