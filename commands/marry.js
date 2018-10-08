55c9a19e6a47a268912ce913b2f4e8628b72f814
railinator4903 <railinator4903@gmail.com>
2018-10-08 18:23:39 +0000

    ring does not get taken away if proposal rejected

--- commands/marry.js
+++ commands/marry.js
@@ -5,28 +5,25 @@ exports.run = (client, msg, args) => {
         if (!profile) return msg.channel.createMessage(':exclamation: │ You haven\'t set up your profile!')
         if (profile.marriedTo) return msg.channel.createMessage(':exclamation: │ Yor\'re already married!')
         if (1 > profile.itemRing) return msg.channel.createMessage(':exclamation: │ You need a ring!');
-        client.r.table('users').get(msg.author.id).update({
-            itemRing: profile.itemRing - 1
-        }).run().then(newProfile => {
-            resolveUser(client, args.join(' ')).then(async toMarry => {
-                if (toMarry.bot) return msg.channel.createMessage(':exclamation: │ You can\'t marry a bot!')
-                msg.channel.createMessage(`<@${toMarry.id}>, do you accept <@${msg.author.id}>'s proposal?`)
-                let responses = await msg.channel.awaitMessages(m => m.author.id === toMarry.id, { time: 10000, maxMatches: 1 });
+        resolveUser(client, args.join(' ')).then(async toMarry => {
+            if (toMarry.bot) return msg.channel.createMessage(':exclamation: │ You can\'t marry a bot!')
+            msg.channel.createMessage(`<@${toMarry.id}>, do you accept <@${msg.author.id}>'s proposal?`)
+            let responses = await msg.channel.awaitMessages(m => m.author.id === toMarry.id, { time: 10000, maxMatches: 1 });
+            
+            if (responses[0].content.toLowerCase() == "yes") {
+                msg.channel.createMessage(`<@${msg.author.id}>, <@${toMarry.id}> accepted your proposal!`)
                 
-                if (responses[0].content.toLowerCase() == "yes") {
-                    msg.channel.createMessage(`<@${msg.author.id}>, <@${toMarry.id}> accepted your proposal!`)
-                    
-                    client.r.table('users').get(msg.author.id).update({
-                        marriedTo: toMarry.id
-                    }).run().then(married => {
-                        client.r.table('users').get(toMarry.id).update({
-                            marriedTo: msg.author.id
-                        }).run().then(done => {
-                            msg.channel.createMessage(`<@${msg.author.id}> and <@${toMarry.id}> are married now! :tada:`)
-                        })
+                client.r.table('users').get(msg.author.id).update({
+                    marriedTo: toMarry.id,
+                    itemRing: profile.itemRing - 1
+                }).run().then(married => {
+                    client.r.table('users').get(toMarry.id).update({
+                        marriedTo: msg.author.id
+                    }).run().then(done => {
+                        msg.channel.createMessage(`<@${msg.author.id}> and <@${toMarry.id}> are married now! :tada:`)
                     })
-                }
-            })
+                })
+            }
         })
     })
 }
