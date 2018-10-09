3b62f3a244706315f7d1be8c687b5a2fea1c3789
railinator4903 <railinator4903@gmail.com>
2018-10-09 13:29:46 +0000

    added donger command

--- commands/donger.js
+++ commands/donger.js
@@ -3,7 +3,7 @@ const getEmbedColor = require('../util/getHighestRoleColor.js')
 exports.run = (client, msg, args) => {
     switch (args.shift()) {
         case 'info':
-            client.r.table('dongers').get(args[0]).run().then(donger => {
+            client.r.table('dongers').get(args.join(' ').toLowerCase()).run().then(donger => {
                 if (!donger) return msg.channel.createMessage(':exclamation: │ That donger is not in the database! Join the support server to suggest it!')
                 msg.channel.createMessage({
                     embed: {
@@ -29,7 +29,7 @@ exports.run = (client, msg, args) => {
             })
             break;
         case 'list':
-            client.r.table('dongers').filter({ category: args[0] }).run().then(category => {
+            client.r.table('dongers').filter({ category: args.join(' ').toLowerCase() }).run().then(category => {
                 msg.channel.createMessage(`**Dongers in category ${args[0]}:**\n${category.map(c => `${c.id}\n`)}`)
             })
     }
