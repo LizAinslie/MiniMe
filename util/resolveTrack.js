3c2f75dccf7ee1d5537e2d6cacf34982ed9e5cef
railinator4903 <railinator4903@gmail.com>
2018-10-16 02:35:25 +0000

    added logging >:(

--- util/resolveTrack.js
+++ util/resolveTrack.js
@@ -5,6 +5,7 @@ module.exports = query => {
 	return new Promise((resolve, reject) => {
 		snekfetch.get(`http://${config.lavalink.nodes[0].host}:${config.lavalink.nodes[0].port}/loadtracks?identifier=${query}`).set('Authorization', config.lavalink.nodes[0].password).then(result => {
 			console.log(result.body)
+			console.log(result.body.tracks[0].info)
 			resolve(result.body)
 		}).catch(error => reject(error))
 	})
