d1641cf0ed67737103ace8ef449d40b19bac5d13
railinator4903 <railinator4903@gmail.com>
2018-10-16 01:17:46 +0000

    fixed resolveTrack uril

--- util/resolveTrack.js
+++ util/resolveTrack.js
@@ -3,7 +3,7 @@ const config = require('../config.json')
 
 module.exports = query => {
 	return new Promise((resolve, reject) => {
-		snekfetch.get('http://${config.lavalink.nodes[0].host}:{config.lavalink.nodes[0].port}/loadtracks?identifier=${query}').set('Authorization', config.lavalink.nodes[0].password).then(result => {
+		snekfetch.get(`http://${config.lavalink.nodes[0].host}:{config.lavalink.nodes[0].port}/loadtracks?identifier=${query}`).set('Authorization', config.lavalink.nodes[0].password).then(result => {
 			resolve(result.body)
 		}).catch(error => reject(error))
 	})
