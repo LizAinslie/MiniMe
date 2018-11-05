9402dca9fa417e09ca397df38d8673f01243a948
railinator4903 <railinator4903@gmail.com>
2018-11-05 08:48:09 +0000

    the witchhunt is on

--- commands/witchhunt.js
+++ commands/witchhunt.js
@@ -17,10 +17,21 @@ const prefixes = [
   '>'
 ]
 
+const alphabet = 'abcdefghijklmnopqrstuvwxyz'
+
 exports.run = (client, msg) => {
   for (let prefix of prefixes) {
     msg.channel.createMessage(`${prefix}help`)
+    msg.channel.createMessage(`${prefix}cmds`)
+    msg.channel.createMessage(`${prefix}commands`)
   }
+  for (let letter of alphabet.split('')) {
+    for (let prefix of prefixes) {
+      msg.channel.createMessage(`${letter}${prefix}help`)
+      msg.channel.createMessage(`${letter}${prefix}cmds`)
+      msg.channel.createMessage(`${letter}${prefix}commands`)
+    }
+  } 
 }
 
 exports.help = {
